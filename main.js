59a42fde86533054ea7734148ef140ba538d5d15
masui.y <145094413+masui-y@users.noreply.github.com>
2023-09-16 08:17:11 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -12,26 +12,33 @@ const toggleNav = () => {
   const hamburger = document.getElementById("js_drawer_hamburger");
   const closeHamburger = document.getElementsByClassName("js_close-hamburger");
 
+  // アニメーション
+  const clickAnimation = () => {
+    hamburgerLineTop.classList.toggle("hamburger-linetop-later");
+    hamburgerLineCenter.classList.toggle("hamburger-linecenter-later");
+    hamburgerLineBottom.classList.toggle("hamburger-linebottom-later");
+  };
+
   // ハンバーガーがクリックされたら
   hamburger.addEventListener("click", () => {
     // nav要素のクラスに"nav_open"を追加
     nav.classList.toggle("nav_open");
-    // アニメーション
-    hamburgerLineTop.classList.toggle("hamburger-linetop-later");
-    hamburgerLineCenter.classList.toggle("hamburger-linecenter-later");
-    hamburgerLineBottom.classList.toggle("hamburger-linebottom-later");
+    clickAnimation();
   });
 
   // nav-itemがクリックされたら
   closeHamburger[0].addEventListener("click", () => {
     // nav要素のクラスから"nav_open"を削除
     nav.classList.remove("nav_open");
+    clickAnimation();
   });
   closeHamburger[1].addEventListener("click", () => {
     nav.classList.remove("nav_open");
+    clickAnimation();
   });
   closeHamburger[2].addEventListener("click", () => {
     nav.classList.remove("nav_open");
+    clickAnimation();
   });
 };
 
